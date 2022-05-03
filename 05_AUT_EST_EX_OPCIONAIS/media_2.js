7ac9ad354ecfddf7f9ba1772455a464193a38ad8
cmtabr <Caio.abreu@sou.inteli.edu.br>
2022-05-03 19:24:42 -0300

    Update media_2.js

---
+++ 05_AUT_EST_EX_OPCIONAIS/media_2.js
@@ -0,0 +1,6 @@
+var a = parseFloat(lines.shift()) * 2;
+var b = parseFloat(lines.shift()) * 3;
+var c = parseFloat(lines.shift()) * 5;
+var x = (a + b + c)/10 
+
+console.log('MEDIA = ' + x.toFixed(2))
