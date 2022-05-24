43d608b2efe5da2462a25afcbd55811d0ccad1a3
cmtabr <Caio.abreu@sou.inteli.edu.br>
2022-05-23 22:19:25 -0300

    att

--- 03_AUT_EST_ENTREGA/Semana 5/public/comment.js
+++ 03_AUT_EST_ENTREGA/Semana 5/public/comment.js
@@ -7,16 +7,26 @@ function fun() {
 
 var darkmode = 0
 function botao(){
+    if(darkmode==1){
+        darkmode = 0
+        $("body").hide()
+        $("body").fadeIn("20000")
+        $("#corpo").css({"color": "black"})
+        $("#corpo").css({"backgroundColor": "white"})
+        $(".top-selection").css({"backgroundColor": "#151b29"})
+        $("body").css({"background-image": "radial-gradient(#c7c7c7 25%, #c7c7c7 74%)"})
+        $(".main").css({none})
+        return darkmode
+    }
     if(darkmode==0){
         darkmode = 1
         $("body").hide()
         $("body").fadeIn("20000")
         $("#corpo").css({"color": "white"})
-        $("#corpo").css({"backgroundColor": "black"})
-        $(".top-selection").css({"backgroundColor": "#151b29"})
-        $("body").css({"background-image": "radial-gradient(#121212 25%, rgb(53, 47, 232) 90%)"})
+        $("#corpo").css({"backgroundColor": "#121212"})
+        $(".top-selection").css({"backgroundColor": "rgb(80, 17, 17)"})
+        $("body").css({"background-image": "radial-gradient(rgb(18, 18, 18) 25%, rgb(181 47 47) 90%"})
         $(".main").css({none})
         return darkmode
     }
-
 }
